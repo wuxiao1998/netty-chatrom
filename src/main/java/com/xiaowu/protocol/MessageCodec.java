1c1ce92063219b71f18caa770ee0d5498615cd60
xiao.wu <xiao.wu@sumscope.com>
2023-04-07 12:34:17 +0800

    add MessageCodec decode method

--- src/main/java/com/xiaowu/protocol/MessageCodec.java
+++ src/main/java/com/xiaowu/protocol/MessageCodec.java
@@ -4,8 +4,11 @@ import com.xiaowu.message.Message;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageCodec;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
 
@@ -13,14 +16,15 @@ import java.util.List;
  * 自定义协议
  * 解码和编码
  */
+@Slf4j
 public class MessageCodec extends ByteToMessageCodec<Message> {
-    private final static String MAGIC_NUM = "CHATROM";
+    private final static byte[] MAGIC_NUM = {1, 2, 3, 4};
     private final static byte Version = 1;
 
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, Message message, ByteBuf byteBuf) throws Exception {
         // 1.魔数 7字节
-        byteBuf.writeBytes(MAGIC_NUM.getBytes());
+        byteBuf.writeBytes(MAGIC_NUM);
         // 2.协议版本 1
         byteBuf.writeByte(Version);
         // 3.序列化算法 0:jdk 1:json 1
@@ -42,6 +46,29 @@ public class MessageCodec extends ByteToMessageCodec<Message> {
 
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
-
+        // 1.获取魔数
+        ByteBuf magicNum = byteBuf.readBytes(MAGIC_NUM.length);
+        // 2.获取协议版本
+        byte version = byteBuf.readByte();
+        // 3.获取序列化算法
+        byte serializeType = byteBuf.readByte();
+        // 4.获取指令类型
+        byte messageType = byteBuf.readByte();
+        // 5.获取序列号
+        int sequenceId = byteBuf.readInt();
+        // 6.获取长度
+        int length = byteBuf.readInt();
+        // 7.获取内容
+        byte[] bytes = new byte[length];
+        byteBuf.readBytes(bytes, 0, length);
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+        Message message = (Message) ois.readObject();
+        log.debug(
+                "magicNum:{},version:{},serializeType:{},messageType:{},sequenceId:{},length:{}",
+                magicNum, version, serializeType, messageType, sequenceId, length
+        );
+        log.debug("message is {}", message);
+        // 放入list，传入下一个handler
+        list.add(message);
     }
 }
